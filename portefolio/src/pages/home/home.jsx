4822c6edab231aadcf54bf002a540efe7f707a53
YUHI <dennys.lemoine.1@gmail.com>
2023-05-17 19:53:30 +0200

    start home page

--- portefolio/src/pages/home/home.jsx
+++ portefolio/src/pages/home/home.jsx
@@ -7,7 +7,7 @@ import starLogo from '../../assets/shining.png'
 function Home() {
     return(
         <>
-            <div>
+            <section className='sectionIntroducing'>
                 <div className='containerPictureHome'>
                     <div className='containerPicture'>
                         <img className='pictureHome' src={profilPicture} alt="Photo de Dennys Lemoine"/>
@@ -22,9 +22,9 @@ function Home() {
                         <img className='imgH1' src={starLogo} alt="Logo de scintillement"/>
                     </div>
                     <h2 className='introducingH2'>DÉVELOPPEUR WEB FRONT-END</h2>
-                    <p className='introducingP'>Expérience dans la conception de sites web  responsives et accessibles. Disponible pour des projets de  développement web intéressants et stimulants. |</p>
+                    <p className='introducingP'>Expérience dans la conception de sites web  responsives et accessibles. Disponible pour des projets de  développement web intéressants et stimulants.</p>
                 </div>
-            </div>
+            </section>
         </>
     )
 }
