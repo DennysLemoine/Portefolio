e879a59fcb5df62a88e31c857870daaddad0cafc
YUHI <dennys.lemoine.1@gmail.com>
2023-05-20 16:08:41 +0200

    about component in progress

---
+++ portefolio/src/components/about/about.jsx
@@ -0,0 +1,24 @@
+import about from './about.css'
+
+import logoAbout from '../../assets/information.png'
+
+function About() {
+    return (
+        <>
+            <section className='sectionAbout'>
+                <span className='sphere04'/>
+                <span className='sphere05'/>
+                <span className='sphere06'/>
+                <h2>À propos</h2>
+                <h3>DÉVELOPPEUR WEB FRONT-END</h3>
+                <div>
+                    <p>Suite à plusieurs années dans l'univers de la 3D, j'ai décidé de me lancer dans une reconversion au
+                        métier de développeur front-end ! Toujours avive de créativité, je souhaite désormais pouvoir amener
+                        mes compétences dans la réalisation de sites web !</p>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default About;
