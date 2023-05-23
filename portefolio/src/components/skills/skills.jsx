f12d743ad6891c312bd62d0c5d485de4ed12f1c6
YUHI <dennys.lemoine.1@gmail.com>
2023-05-23 12:46:58 +0200

    continue skills section

--- portefolio/src/components/skills/skills.jsx
+++ portefolio/src/components/skills/skills.jsx
@@ -1,8 +1,17 @@
+import React, {useState} from "react";
+
 import skills from './skills.css'
 
 import skillLogo from '../../assets/creative.png'
 
 function Skills() {
+
+    const [visibleDiv, setVisibleDiv] = useState(false);
+
+    const toggleSkills = () => {
+        setVisibleDiv(!visibleDiv);
+    };
+
     return (
         <section className='sectionSkills'>
             <span className='sphere07'/>
@@ -12,125 +21,134 @@ function Skills() {
                 <h2>COMPÉTENCES</h2>
                 <img src={skillLogo} alt="Logo créatif"/>
             </div>
-            <div className='containerSkills01'>
-                <div className='containerSkill'>
-                    <div className='containerSkillP'>
-                        <p>0%</p>
-                        <p>100%</p>
+            <div className='containerSkills'>
+                <div className={visibleDiv ? 'containerSkills01 hide' : 'containerSkills01'}>
+                    <div className='containerSkill'>
+                        <div className='containerSkillP'>
+                            <p>0%</p>
+                            <p>100%</p>
+                        </div>
+                        <div className='divSkillBorder'>
+                            <div className='divSkillHtml'>
+                                <p>HTML</p>
+                                <p>80%</p>
+                            </div>
+                        </div>
                     </div>
-                    <div className='divSkillBorder'>
-                        <div className='divSkillHtml'>
-                            <p>HTML</p>
-                            <p>80%</p>
+                    <div className='containerSkill'>
+                        <div className='containerSkillP'>
+                            <p>0%</p>
+                            <p>100%</p>
+                        </div>
+                        <div className='divSkillBorder'>
+                            <div className='divSkillCss'>
+                                <p>CSS</p>
+                                <p>80%</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='containerSkill'>
+                        <div className='containerSkillP'>
+                            <p>0%</p>
+                            <p>100%</p>
+                        </div>
+                        <div className='divSkillBorder'>
+                            <div className='divSkillJs'>
+                                <p>JAVASCRIPT</p>
+                                <p>60%</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='containerSkill'>
+                        <div className='containerSkillP'>
+                            <p>0%</p>
+                            <p>100%</p>
+                        </div>
+                        <div className='divSkillBorder'>
+                            <div className='divSkillReact'>
+                                <p>REACT</p>
+                                <p>60%</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='containerSkill'>
+                        <div className='containerSkillP'>
+                            <p>0%</p>
+                            <p>100%</p>
+                        </div>
+                        <div className='divSkillBorder'>
+                            <div className='divSkillNode'>
+                                <p>NODE</p>
+                                <p>30%</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='containerSkill'>
+                        <div className='containerSkillP'>
+                            <p>0%</p>
+                            <p>100%</p>
+                        </div>
+                        <div className='divSkillBorder'>
+                            <div className='divSkillSeo'>
+                                <p>SEO</p>
+                                <p>50%</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='containerSkill'>
+                        <div className='containerSkillP'>
+                            <p>0%</p>
+                            <p>100%</p>
+                        </div>
+                        <div className='divSkillBorder'>
+                            <div className='divSkillPhotoshop'>
+                                <p>PHOTOSHOP</p>
+                                <p>60%</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='containerSkill'>
+                        <div className='containerSkillP'>
+                            <p>0%</p>
+                            <p>100%</p>
+                        </div>
+                        <div className='divSkillBorder'>
+                            <div className='divSkillInfographie'>
+                                <p>INFOGRAPHIE</p>
+                                <p>70%</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='containerSkill'>
+                        <div className='containerSkillP'>
+                            <p>0%</p>
+                            <p>100%</p>
+                        </div>
+                        <div className='divSkillBorder'>
+                            <div className='divSkill3d'>
+                                <p>3D</p>
+                                <p>80%</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className='containerSkill'>
-                    <div className='containerSkillP'>
-                        <p>0%</p>
-                        <p>100%</p>
-                    </div>
-                    <div className='divSkillBorder'>
-                        <div className='divSkillCss'>
-                            <p>CSS</p>
-                            <p>80%</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='containerSkill'>
-                    <div className='containerSkillP'>
-                        <p>0%</p>
-                        <p>100%</p>
-                    </div>
-                    <div className='divSkillBorder'>
-                        <div className='divSkillJs'>
-                            <p>JAVASCRIPT</p>
-                            <p>60%</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='containerSkill'>
-                    <div className='containerSkillP'>
-                        <p>0%</p>
-                        <p>100%</p>
-                    </div>
-                    <div className='divSkillBorder'>
-                        <div className='divSkillReact'>
-                            <p>REACT</p>
-                            <p>60%</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='containerSkill'>
-                    <div className='containerSkillP'>
-                        <p>0%</p>
-                        <p>100%</p>
-                    </div>
-                    <div className='divSkillBorder'>
-                        <div className='divSkillNode'>
-                            <p>NODE</p>
-                            <p>30%</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='containerSkill'>
-                    <div className='containerSkillP'>
-                        <p>0%</p>
-                        <p>100%</p>
-                    </div>
-                    <div className='divSkillBorder'>
-                        <div className='divSkillSeo'>
-                            <p>SEO</p>
-                            <p>50%</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='containerSkill'>
-                    <div className='containerSkillP'>
-                        <p>0%</p>
-                        <p>100%</p>
-                    </div>
-                    <div className='divSkillBorder'>
-                        <div className='divSkillPhotoshop'>
-                            <p>PHOTOSHOP</p>
-                            <p>60%</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='containerSkill'>
-                    <div className='containerSkillP'>
-                        <p>0%</p>
-                        <p>100%</p>
-                    </div>
-                    <div className='divSkillBorder'>
-                        <div className='divSkillInfographie'>
-                            <p>INFOGRAPHIE</p>
-                            <p>70%</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='containerSkill'>
-                    <div className='containerSkillP'>
-                        <p>0%</p>
-                        <p>100%</p>
-                    </div>
-                    <div className='divSkillBorder'>
-                        <div className='divSkill3d'>
-                            <p>3D</p>
-                            <p>80%</p>
+                <div className={visibleDiv ? 'containerSkills02' : 'containerSkills02 hide'}>
+                    <div className='containerSkillGraduate'>
+                        <div className='containerGraduateRelatif'>
+                            <span className='lineSkill'></span>
+                            <span className='circleSkillP01'></span>
+                            <span className='circleSkillP02'></span>
+                            <span className='circleSkillP03'></span>
+                            <p className='skillBts'>BTS MUC<br/>2017</p>
+                            <p className='skill3d'>MODÉLISATION / ANIMATION 3D<br/>2021</p>
+                            <p className='skillDev'>DÉVELOPPEUR WEB<br/>2023</p>
                         </div>
                     </div>
                 </div>
                 <span className='containerSkillsButtons'>
-                    <span className='button1'></span>
-                    <span className='button2'></span>
+                    <span className='button1' onClick={toggleSkills}></span>
+                    <span className='button2' onClick={toggleSkills}></span>
                 </span>
-            </div>
-            <div className='containerSkills02'>
-                <div className='containerSkillP'>
-                    <p>BTS MUC<br/>2017</p>
-                    <p>DÉVELOPPEUR WEB<br/>2023</p>
-                </div>
             </div>
         </section>
     )
