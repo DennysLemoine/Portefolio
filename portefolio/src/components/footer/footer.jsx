9b38289f956d2bf35a8ac1688da8081644220212
YUHI <dennys.lemoine.1@gmail.com>
2023-06-01 13:28:56 +0200

    fix problm

--- portefolio/src/components/footer/footer.jsx
+++ portefolio/src/components/footer/footer.jsx
@@ -17,7 +17,7 @@ function Footer() {
                 <Link to="https://www.linkedin.com/in/dennys-lemoine-b4873313b/" target="_blank" className='footerLogo'>
                     <img src={linkedinLogoWhite} alt="Logo/Lien Linkedin"/>
                 </Link>
-                <Link to="mailto:dennys.lemoine.1@gmail.com" target="_blank" className='footerLogo'>
+                <Link to="mailto:dennys.lemoine.1@gmail.com" className='footerLogo'>
                     <img src={mailLogo} alt="Logo/Lien Mail"/>
                 </Link>
                 <Link to="https://github.com/DennysLemoine/" target="_blank" className='footerLogo'>
